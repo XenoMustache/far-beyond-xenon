536e3a09c77d69ce8b490cacdab00b90ae346cb7
Joshua Clift <xenomustache@gmail.com>
2020-08-19 13:34:28 -0700

    Added damage property to ProjectileCollisionBox

--- Objects/ProjectileCollisionBox.cs
+++ Objects/ProjectileCollisionBox.cs
@@ -4,10 +4,13 @@ using SFML.System;
 
 namespace FarBeyond.Objects {
 	public class ProjectileCollisionBox : CollisionBox {
+		public float damage;
+
 		public ProjectileCollisionBox(Entity parent, Vector2f position, Vector2f size, Color color) : base(parent, position, size, color) { }
 
 		public override void OnColliderEnter(CollisionBox collided) {
 			var obj = collided.GetParent();
+			obj.health -= damage;
 
 			base.OnColliderEnter(collided);
 		}
