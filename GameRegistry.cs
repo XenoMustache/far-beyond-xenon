6f0726df73d19524d85db44921b14c7a1d3376c0
Joshua Clift <xenomustache@gmail.com>
2020-09-07 10:31:39 -0700

    GameRegistry asset functions

--- GameRegistry.cs
+++ GameRegistry.cs
@@ -4,14 +4,36 @@ namespace FarBeyond {
 	public class GameRegistry {
 		public static Texture civShipsTexture, bulletsTexture, pirateShipsTexture;
 		public static Shader starfield, explosion;
+		public static Font uiDefault, uiStylized;
+
+		static string location = "Resources";
 
 		public static void Init() {
-			civShipsTexture = new Texture("Resources\\Textures\\civ_ships.png");
-			pirateShipsTexture = new Texture("Resources\\Textures\\pirate_ships.png");
-			bulletsTexture = new Texture("Resources\\Textures\\bullets.png");
+			civShipsTexture = addTexture("civ_ships.png");
+			pirateShipsTexture = addTexture("pirate_ships.png");
+			bulletsTexture = addTexture("bullets.png");
 
-			starfield = new Shader(null, null, "Resources\\Shaders\\starfield.glsl");
-			explosion = new Shader(null, null, "Resources\\Shaders\\explode.glsl");
+			starfield = addFragShader("starfield.glsl");
+			explosion = addFragShader("explode.glsl");
+
+			uiDefault = addFont("Monaco.ttf");
+			uiStylized = addFont("KennyRocket.ttf");
+		}
+
+		static Texture addTexture(string name) {
+			var texture = new Texture($"{location}\\Textures\\{name}");
+			return texture;
+		}
+
+		static Shader addFragShader(string fragment) {
+			var locale = $"{location}\\Shaders\\";
+			var shader = new Shader(null, null, $"{locale}{fragment}");
+			return shader;
+		}
+
+		static Font addFont(string name) {
+			var font = new Font($"{location}\\Fonts\\{name}");
+			return font;
 		}
 	}
 }
