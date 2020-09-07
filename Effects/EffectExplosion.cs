4eff90a24fbc82805d158a585c0abbf4e3871c55
Joshua Clift <xenomustache@gmail.com>
2020-09-07 11:24:01 -0700

    Explosion customization, cleaned shader code

--- Effects/EffectExplosion.cs
+++ Effects/EffectExplosion.cs
@@ -8,18 +8,23 @@ namespace FarBeyond.Effects {
 		Shader shader;
 		Clock clock;
 
-		public EffectExplosion(float seed, float particleCount, float pixelResolution, Vec4 color) {
+		public EffectExplosion(float seed, float particleCount, float pixelResolution, float spread, float travelSpeed, float fadeIntesity, Vec2 direction, Vec4 color) {
 			clock = new Clock();
 			shader = GameRegistry.explosion;
 
+			Vec2[] iDirection = new Vec2[] { direction };
 			Vec3[] iResolution = new Vec3[] { new Vec3(800, 450, 0) };
 			Vec4[] iColor = new Vec4[] { color };
 
+			shader.SetUniformArray("iDirection", iDirection);
 			shader.SetUniformArray("iResolution", iResolution);
 			shader.SetUniformArray("iColor", iColor);
 			shader.SetUniform("iSeed", seed);
 			shader.SetUniform("iParticles", particleCount);
 			shader.SetUniform("iPixelResolution", pixelResolution);
+			shader.SetUniform("iSpread", spread);
+			shader.SetUniform("iTravelSpeed", travelSpeed);
+			shader.SetUniform("iFadeIntensity", fadeIntesity);
 		}
 
 		protected override void OnUpdate(double deltaTime) {
@@ -28,8 +33,6 @@ namespace FarBeyond.Effects {
 			shader.SetUniform("iTime", time);
 		}
 
-		protected override void OnDraw(RenderTarget target, RenderStates states) {
-
-		}
+		protected override void OnDraw(RenderTarget target, RenderStates states) { }
 	}
 }
--- Resources/Shaders/explode.glsl
+++ Resources/Shaders/explode.glsl
@@ -2,21 +2,30 @@ uniform float iTime;
 uniform float iSeed;
 uniform float iParticles;
 uniform float iPixelResolution;
-// Change to vec2
-uniform float iDirection;
+uniform float iSpread;
+uniform float iTravelSpeed;
+uniform float iFadeIntensity;
 
+uniform vec2 iDirection;
 uniform vec3 iResolution;
-// 5.0, 0.5, 0.1, 1.0
+// RED: 5.0, 0.5, 0.1, 1.0
+// CYAN: 0.1, 0.4, 0.5, 1.0
 uniform vec4 iColor;
-
 // 0.32
 float seed = iSeed;
 // 32.
 float particles = iParticles;
 // 32.
 float res = iPixelResolution;
-// 0.
-float gravity = 0.0;
+// 0.4
+float spread = iSpread;
+// 2.0
+float travelSpeed = iTravelSpeed;
+// 1.0
+float fadeIntensity = iFadeIntensity;
+
+vec2 direction = iDirection;
+vec4 color = iColor;
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord);
 
@@ -26,31 +35,28 @@ void main() {
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	vec2 uv = (-iResolution.xy + 2.0 * fragCoord.xy) / iResolution.y;
-
    	float clr = 0.0;  
-    float timecycle = iTime - floor(iTime);  
-
-    seed = (seed + floor(iTime));
-
-    float invres = 1.0 / res;
-    float invparticles = 0.5 / particles;
-    
-    for(float i = 0.0; i < particles; i += 0.1) {
-		seed+=i+tan(seed);
-        vec2 tPos = (vec2(cos(seed), sin(seed))) * i * invparticles;
-        
-        vec2 pPos = vec2(0.0, 0.0);
-        pPos.x = ((tPos.x) * timecycle * 2.0);
-		pPos.y = -gravity * (timecycle*timecycle)+tPos.y * timecycle * 2.0 + pPos.y;
-        
-        pPos = floor(pPos * res) * invres;
-
-    	vec2 p1 = pPos;
-    	vec4 r1 = vec4(vec2(step(p1, uv)), 1.0 - vec2(step(p1 + invres, uv)));
-    	float px1 = r1.x * r1.y * r1.z * r1.w;
-        
-	    clr += px1 * (sin(iTime + i) + 1.0);
-    }
-    
-	fragColor = vec4(clr * (1.0 - timecycle)) * iColor;
+	float timecycle = iTime - floor(iTime);  
+	seed = (seed + floor(iTime));
+	
+	float invres = 1.0 / res;
+	float invparticles = spread / particles;
+	
+	for(float i = 0.0; i < particles; i += 0.1) {
+		seed += i + tan(seed);
+		vec2 tPos = (vec2(cos(seed), sin(seed))) * i * invparticles;
+		
+		vec2 pPos = vec2(0.0, 0.0);
+		pPos.x = -direction.x * (timecycle * timecycle) + tPos.x * timecycle * travelSpeed + pPos.x;
+		pPos.y = -direction.y * (timecycle * timecycle) + tPos.y * timecycle * travelSpeed + pPos.y;
+		
+		pPos = floor(pPos * res) * invres;
+		vec2 p1 = pPos;
+		vec4 r1 = vec4(vec2(step(p1, uv)), 1.0 - vec2(step(p1 + invres,uv)));
+		float px1 = r1.x * r1.y * r1.z * r1.w;
+		
+		clr += px1 * (sin(iTime + i) + 1.0);
+	}
+	
+	fragColor = vec4(clr * (fadeIntensity - timecycle)) * color;
 }
